e1f14e9a56ede6c12c315197ad001eb7d228378b
Dixittushar <dixittushar90@example.com>
2023-12-27 19:52:20 +0530

    brand page first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Navigation from "./components/Navigation";
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
